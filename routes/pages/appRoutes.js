f0ae861ea7686e93113f369f94104236b7989119
3mpirical <3mpirical10510@gmail.com>
2018-12-26 04:36:44 -0700

    changed app route from /app to just /. This will be changed as needed

--- routes/pages/appRoutes.js
+++ routes/pages/appRoutes.js
@@ -1,7 +1,7 @@
 const   express = require("express"),
         router  = express.Router();
 
-router.get("/app", (req, res) => {
+router.get("/", (req, res) => {
     res.status(200)
         .render("index");
 });
