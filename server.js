c03d29254462843b74096042bce21a3eeae781dc
3mpirical <3mpirical10510@gmail.com>
2018-12-30 17:33:21 -0700

    added mongoose and conection setup to server, and required event api routes

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const   express      = require("express"),
         app          = express(),
+        mongoose     = require("mongoose");
         bodyParser   = require("body-parser"),
         hbs          = require("hbs"),
-        appPageRoute = require("./routes/pages/appRoutes");
+
+        appPageRoute = require("./routes/pages/appRoutes"),
+        eventRoutes  = require("./routes/api/events/eventRoutes");
 
 // SETUP
 const PORT = process.env.PORT || 3005;
+const MONGODB_URI = "mongodb://127.0.0.1:27017/projectify"; //process.env.MONGODB_URI ||
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
+    .then(() => console.log("MongoDB Connected Successfully"))
+    .catch((err) => console.log(`Failed To Connect To MongoDB: \n${err}`));
 app.set("view engine", "hbs");
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -15,6 +22,7 @@ hbs.registerPartials(__dirname + "/views/partials");
 
 // ROUTES
 app.use(appPageRoute);
+app.use(eventRoutes);
 
 //error route
 app.get("*", (req, res) => {
