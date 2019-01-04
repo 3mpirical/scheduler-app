65a3ac6943a301b51fa0001f7484dad905251d17
3mpirical <3mpirical10510@gmail.com>
2019-01-03 19:12:25 -0700

    changed indentation on promises.

--- public/js/app/models/_eventModel.js
+++ public/js/app/models/_eventModel.js
@@ -14,48 +14,48 @@ class Event {
     static save(eventObj) {
         return new Promise((resolve, reject) => {
             axios.post("/api/events", eventObj)
-                .then((eventData) => resolve(eventData.data.data))
-                .catch((err) => reject(err.response.data));
+            .then((eventData) => resolve(eventData.data.data))
+            .catch((err) => reject(err.response.data));
         });
     }
 
     static findAll() {
         return new Promise((resolve, reject) => {
             axios.get("/api/events")
-                .then((eventData) => resolve(eventData.data.data))
-                .catch((err) => reject(err.response.data));
+            .then((eventData) => resolve(eventData.data.data))
+            .catch((err) => reject(err.response.data));
         });
     }
 
     static findByMonth(month) {
         return new Promise((resolve, reject) => {
             axios.get(`/api/events/${month}`)
-                .then((eventData) => resolve(eventData.data.data))
-                .catch((err) => reject(err.response.data));
+            .then((eventData) => resolve(eventData.data.data))
+            .catch((err) => reject(err.response.data));
         });
     }
 
     static findById(_id) {
         return new Promise((resolve, reject) => {
             axios.get(`/api/events/${_id}`)
-                .then((eventData) => resolve(eventData.data.data))
-                .catch((err) => reject(err.response.data));
+            .then((eventData) => resolve(eventData.data.data))
+            .catch((err) => reject(err.response.data));
         });
     }
 
     static findByIdAndUpdate(_id, eventObj){
         return new Promise((resolve, reject) => {
             axios.put(`/api/events/${_id}`, eventObj)
-                .then((eventData) => resolve(eventData.data.data))
-                .catch((err) => reject(err.response.data));
+            .then((eventData) => resolve(eventData.data.data))
+            .catch((err) => reject(err.response.data));
         });
 
     }
     static findByIdAndDelete(_id){
         return new Promise((resolve, reject) => {
             axios.delete(`/api/events/${_id}`)
-                .then((eventData) => resolve(eventData.data))
-                .catch((err) => reject(err.response.data));
+            .then((eventData) => resolve(eventData.data))
+            .catch((err) => reject(err.response.data));
         });
     }
 }
