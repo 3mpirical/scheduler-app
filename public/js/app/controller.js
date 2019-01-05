f9c6b3681cf1cd64507ebb5874c154241faedb8b
3mpirical <3mpirical10510@gmail.com>
2019-01-04 21:11:18 -0700

    added experimental swiperight event listener to test

--- public/js/app/controller.js
+++ public/js/app/controller.js
@@ -2,6 +2,8 @@ import { MDL } from "./models/model";
 import { VIEW } from "./views/view";
 import { state } from "./state";
 import { elements } from "./elements";
+import hammerjs from "hammerjs";
+
 
 
 const CTRL = (function(MDL, VIEW, state, elements) {
@@ -73,10 +75,18 @@ const updateSelectedBox = (event) => {
 
 CTRL.initializeCalendar();
 
+//decrement calendar month
 elements.prevMonthButt.addEventListener("click", (event) => {
     CTRL.decrementCalendarMonth();
 });
 
+const decrementSwipe = new Hammer(elements.prevMonthButt);
+decrementSwipe.on("swiperight", (event) => {
+    CTRL.decrementCalendarMonth();
+});
+
+
+//increment calendar month
 elements.nextMonthButt.addEventListener("click", (event) => {
     CTRL.incrementCalendarMonth();
 });
@@ -86,69 +96,3 @@ elements.calendarContainer.addEventListener("click", (event) => {
         CTRL.updateSelectedBox(event);
     }
 });
-
-// const event1 = new MDL.Event({
-//     type: "Pesonal",
-// 	name: "College Graduation",
-//     description: "I did it, and I'm not even dead yet.",
-// 	dateExecuting: {
-// 		day: 31,
-// 		month: 4,
-// 		year: 2019,
-// 		time: 1100
-// 	}
-// });
-// const event2 = new MDL.Event({
-//     type: "Holiday",
-// 	name: "Earth Day",
-//     description: "Time to celebrate the planet",
-// 	dateExecuting: {
-// 		day: 4,
-// 		month: 3,
-// 		year: 2019,
-// 		time: 0
-// 	}
-// });
-
-// MDL.Event.save(event1)
-//     .then((eventData) => {
-//         console.log(`${JSON.stringify(eventData, null, 2)}\n^^^^^Event Saved`);
-//     })
-//     .catch((err) => {
-//         console.log(`Error: Failed To Save Event\n${err}`);
-//     });
-
-// MDL.Event.findAll()
-//     .then((eventData) => {
-//         console.log(`Event Data Found\n${JSON.stringify(eventData, null, 2)}`);
-//     })
-//     .catch((err) => {
-//         console.log(`Error: Data Not Found\n${err}`);
-//     });
-
-// MDL.Event.findById("5c29aff230a171c29d0b13ae")
-//     .then((eventData) => {
-//         console.log(`Event Data Found\n${JSON.stringify(eventData, null, 2)}`);
-//     })
-//     .catch((err) => {
-//         console.log(`Error: Data Not Found\n${err}`);
-//     });
-
-// MDL.Event.findByIdAndUpdate("5c29aff230a171c29d0b13ae", event1)
-//     .then((eventData) => {
-//         console.log(`Event Data Found\n${JSON.stringify(eventData, null, 2)}`);
-//     })
-//     .catch((err) => {
-//         console.log(`Error: ${JSON.stringify(err, null, 2)}`);
-//     });
-// MDL.Event.findByIdAndDelete("5c294873e03ec5a91b31cc7b")
-//     .then((eventData) => {
-//         console.log(`Event Data Deleted\n${JSON.stringify(eventData, null, 2)}`);
-//         return MDL.Event.findAll();
-//     })
-//     .then((eventData) => {
-//         console.log(`All Data Found:\n${eventData}`);
-//     })
-//     .catch((err) => {
-//         console.log(`Error: ${JSON.stringify(err, null, 2)}`);
-//     });
